5898bed08a9680d0888118f294de6394126d8b80
Niadul <niadul@tenbyte.com.my>
2026-04-28 19:59:46 +0600

    before func

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 18
+	// gender := "male"
+	// isPretty := false
+	a := 2
+	b := 8
+
+	sum := a + b
+
+	fmt.Println(sum)
+
+	// switch a {
+	// case 1:
+	// 	fmt.Println("A is 1")
+	// case 2, 3:
+	// 	fmt.Println("A is either 2 or 3")
+	// default:
+	// 	fmt.Println("A is neither 1 nor 2 or 3")
+	// }
+
+	// if age == 18 && gender == "female" {
+	// 	fmt.Println("Welldone")
+	// } else {
+	// 	fmt.Println("Not bad")
+	// }
+	//  if age >= 18 { // greater than or equal to 18?
+	// 	fmt.Println("You're eligible to drive")
+	// } else if age < 18 {
+	// 	fmt.Println("You're not eligible to drive")
+	// } else {
+	// 	fmt.Println("Go home")
+	// }
+	//
+	// if !isPretty {
+	// 	fmt.Println("You're cool")
+	// } else {
+	// 	fmt.Println("Ugly")
+	// }
+	//
+
+}
